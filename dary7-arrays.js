14f0eb2e5d61af3bf65e2222b68449ca56062f7f
Gui Siebert <guilhermebsiebert@gmail.com>
2023-07-05 18:38:59 -0300

    added day7-arrays

---
+++ dary7-arrays.js
@@ -0,0 +1,37 @@
+// Link of Challenge: https://hackerrank.com/challenges/30-arrays/problem?isFullScreen=true
+
+'use strict';
+
+process.stdin.resume();
+process.stdin.setEncoding('utf-8');
+
+let inputString = '';
+let currentLine = 0;
+
+process.stdin.on('data', function(inputStdin) {
+    inputString += inputStdin;
+});
+
+process.stdin.on('end', function() {
+    inputString = inputString.split('\n');
+
+    main();
+});
+
+function readLine() {
+    return inputString[currentLine++];
+}
+
+function reverse(a) {
+    const reversed = a.slice('').reverse().join(' ')
+    console.log(reversed)
+}
+
+
+function main() {
+    const n = parseInt(readLine().trim(), 10);
+
+    const arr = readLine().replace(/\s+$/g, '').split(' ').map(arrTemp => parseInt(arrTemp, 10));
+    
+    reverse(arr)
+}
