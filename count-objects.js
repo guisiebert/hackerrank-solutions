87687ed18414b751157d3c4232dcda36111b822b
Gui Siebert <guilhermebsiebert@gmail.com>
2023-07-06 18:32:00 -0300

    added count-objects

---
+++ count-objects.js
@@ -0,0 +1,12 @@
+// Challenge: https://www.hackerrank.com/challenges/js10-count-objects/problem?isFullScreen=true
+
+function getCount(objects) {
+    let counter = 0
+    
+    for (let i=0; i<objects.length; i++)
+    if (objects[i].x == objects[i].y) {
+        counter++
+    }
+    
+    return counter
+}
